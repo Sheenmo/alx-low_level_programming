67917f1947b60b2d71856c71db8f8a937931e25c
Sheenmo <samkhisa@gmail.com>
2023-10-15 08:09:03 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This is a program that prints line with printf function
+ * Return:0(efficiency)
+ */
+int main(void);
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (o);
+}
